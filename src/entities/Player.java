96736d2253e417a91108917780fd539a83203b43
KirakiStar <mewkchp@gmail.com>
2026-09-21 21:21:53 +0700

    commit 5
    Constants class becomes enum, more Player animations

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -5,17 +5,27 @@ import java.awt.image.BufferedImage;
 
 import main.Game;
 import helper.ResourceLoader;
+import gamestates.Playing;
+import helper.Constants.PlayerState;
 import static main.Game.SCALE;
 import static collision.CollisionChecker.isSolid;
-import gamestates.Playing;
+import static helper.Constants.PlayerState.*;
 
 public class Player extends Entity {
 	private Playing playing;
 	private BufferedImage[][] sprites;
-	private String playerPng = ResourceLoader.PLAYER_SPRITES;
+	private final String playerPng = ResourceLoader.PLAYER_SPRITES;
+	private final int pngRow = 11;
+	private final int pngCol = 4;
+	private PlayerState playerAction;
+	private int aniTick;
+	private int aniIndex;
+	private final int aniSpeed = 15;
+	private boolean facingLeft = false;
+	
 	private static final int WIDTH = (int)(16*Game.SCALE);
 	private static final int HEIGHT = (int)(36*Game.SCALE);
-	private static final float OFFSET_X = 23 * Game.SCALE;
+	private static final float OFFSET_X = 24 * Game.SCALE;
 	private static final float OFFSET_Y = 27 * Game.SCALE;
 	
 	private boolean moving = false;
@@ -24,31 +34,99 @@ public class Player extends Entity {
 	private boolean up;
 	private boolean down;
 	private boolean jump;
-	private float playerSpeed = 1.5f * SCALE;
+	private final float playerSpeed = 1.5f * SCALE;
 	
 	private boolean inAir = false;
 	private int jumpCount = 0;
-	private int maxJump = 2;
+	private final int maxJump = 2;
 	private boolean climbable = false;
-	private float gravity = 0.05f * SCALE;
+	private final float gravity = 0.05f * SCALE;
 	private float airSpeed = 0f;
-	private float jumpSpeed = -2.3f * SCALE;
-	private float fallSpeed = 0.5f * SCALE;
+	private final float jumpSpeed = -2.3f * SCALE;
+	private final float fallSpeed = 0.5f * SCALE;
 
 	public Player(Playing playing, float x, float y) {
 		super(x, y, WIDTH, HEIGHT, OFFSET_X, OFFSET_Y);
 		this.playing = playing;
+		this.playerAction = IDLE;
 		loadAnimations();
+	}
+
+	private void loadAnimations() {
+		BufferedImage img = ResourceLoader.ImagesLoader(playerPng);
+
+		sprites = new BufferedImage[pngRow][pngCol];
+		for(int i = 0; i < pngRow; i++) {
+			for(int j = 0; j < pngCol; j++) {
+				sprites[i][j] = img.getSubimage(j*64, i*64, 64, 64);
+			}
+		}
+	}
+	
+	private void updateAnimationTick() {
+		aniTick++;
+		if (aniTick >= aniSpeed) {
+			aniTick = 0;
+
+			if (playerAction.isLooping()) {
+				aniIndex++;
+				if (aniIndex >= playerAction.getAnimationAmount()) {
+					aniIndex = 0;
+				}
+			} else {
+				if (aniIndex < playerAction.getAnimationAmount() - 1) {
+					aniIndex++;
+				}
+			}
+		}
+	}
+	
+	private void setAnimation(PlayerState newAction) {
+		if (this.playerAction.getId() != newAction.getId()) {
+			this.playerAction = newAction;
+			this.aniTick = 0;
+			this.aniIndex = 0;
+		}
+	}
+	
+	private void updatePlayerAction() {
+		if (inAir) {
+			if (airSpeed < 0) {
+				if (jumpCount < 2) {
+					setAnimation(JUMPING);
+				} else {
+					setAnimation(JUMPING2);
+				}
+			} else {
+				setAnimation(FALLING);
+			}
+		} else if (moving) {
+			setAnimation(RUNNING);
+		} else {
+			setAnimation(IDLE);
+		}
 	}
 
 	public void update() {
 		updatePosition();
 		updateHitbox();
+		updateAnimationTick();
+		updatePlayerAction();
 	}
 
 	public void draw(Graphics2D g2) {
-		g2.drawImage(sprites[0][0], (int)x, (int)y, (int)(64*SCALE), (int)(64*SCALE), null);
-		drawHitbox(g2);
+		int drawX = (int) x;
+		int drawY = (int) y;
+		int drawWidth = (int) (64 * SCALE);
+		int drawHeight = (int) (64 * SCALE);
+		if (facingLeft) {
+			g2.drawImage(sprites[playerAction.getId()][aniIndex], drawX+drawWidth, drawY,
+					-drawWidth, drawHeight, null);
+		} else {
+			g2.drawImage(sprites[playerAction.getId()][aniIndex], drawX, drawY,
+					drawWidth, drawHeight, null);
+		}
+//		drawHitbox(g2);
 	}
 
 	private void updatePosition() {
@@ -57,8 +135,14 @@ public class Player extends Entity {
 		float xSpeed = 0;
 		
 		if (jump) jump();
-		if (left) xSpeed -= playerSpeed;
-		if (right) xSpeed += playerSpeed;
+		if (left) {
+			xSpeed -= playerSpeed;
+			facingLeft = true;
+		}
+		if (right) {
+			xSpeed += playerSpeed;
+			facingLeft = false;
+		}
 		
 		if (!inAir) {
             if (!isSolid(hitbox, x, y + 1, playing.getCollisionData(), playing.getLevelWidth(), playing.getLevelHeight())) {
@@ -127,17 +211,6 @@ public class Player extends Entity {
 //			moving = true;
 //		}
 //	}
-
-	private void loadAnimations() {
-		BufferedImage img = ResourceLoader.ImagesLoader(playerPng);
-
-		sprites = new BufferedImage[4][4];
-		for(int i = 0; i < sprites.length; i++) {
-			for(int j = 0; j < sprites[i].length; j++) {
-				sprites[i][j] = img.getSubimage(j*64, i*64, 64, 64);
-			}
-		}
-	}
 
 	public void resetDirection() {
 		left = false;
--- src/helper/Constants.java
+++ src/helper/Constants.java
@@ -1,21 +1,65 @@
 package helper;
 
 public class Constants {
-	public static class PlayerActionID {
-		public static final int IDLE = 0;
-		public static final int RUNNING = 1;
-		public static final int JUMPING = 2;
-		public static final int JUMPING2 = 3;
-		public static final int FALLING = 4;
-		public static final int HIT_GROUND = 5;
-		public static final int CLIMBING = 6;
-		public static final int ATTACK = 7;
-		public static final int ATTACK_AIR = 8;
-		public static final int ATTACKED = 9;
-		public static final int DEATH = 10;
-		
-		public static int getAnimationAmount(int playerAction) {
-			return 1;
-		}
+	public interface EntityState {
+		int getId();
+		int getAnimationAmount();
 	}
+
+	public enum PlayerState implements EntityState {
+		IDLE(0, 4, true),
+		RUNNING(1, 4, true),
+		JUMPING(2, 1, false),
+		JUMPING2(3, 2, false),
+		FALLING(4, 1, false),
+		HIT_GROUND(5, 1, false),
+		CLIMBING(6, 1, false),
+		ATTACK(7, 1, false),
+		ATTACK_AIR(8, 1, false),
+		ATTACKED(9, 1, false),
+		DEATH(10, 1, false);
+
+		private final int id;
+		private final int animationAmount;
+		private final boolean looping;
+
+		PlayerState(int id, int animationAmount, boolean looping) {
+			this.id = id;
+			this.animationAmount = animationAmount;
+			this.looping = looping;
+		}
+
+		@Override
+		public int getId() { return id; }
+
+		@Override
+		public int getAnimationAmount() { return animationAmount; }
+		
+		public boolean isLooping() { return looping; }
+	}
+	
+//	public static class PlayerActionID {
+//		public static final int IDLE = 0;
+//		public static final int RUNNING = 1;
+//		public static final int JUMPING = 2;
+//		public static final int JUMPING2 = 3;
+//		public static final int FALLING = 4;
+//		public static final int HIT_GROUND = 5;
+//		public static final int CLIMBING = 6;
+//		public static final int ATTACK = 7;
+//		public static final int ATTACK_AIR = 8;
+//		public static final int ATTACKED = 9;
+//		public static final int DEATH = 10;
+//		
+//		public static int getAnimationAmount(int playerAction) {
+//			switch (playerAction) {
+//				case IDLE:
+//				case RUNNING:
+//					return 4;
+//				default: return 1;
+//			}
+//		}
+//	}
+	
+	
 }
